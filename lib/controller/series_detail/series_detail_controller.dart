dcce0421ec96f43f054512a6c97c16a5a75e4ba8
Benedict Howard <hongha137200110@gmail.com>
2022-05-01 23:39:18 +0700

    add api initialization + pagination for series detail screen

--- lib/controller/series_detail/series_detail_controller.dart
+++ lib/controller/series_detail/series_detail_controller.dart
@@ -1,26 +1,36 @@
-import '../../model/Serie.dart';
+import 'dart:convert';
 
-Series buildSerie() {
-  List<SeriesEpisode> testEpisodes = [];
-  for (int i = 0; i < 10; i++) {
-    testEpisodes.add(SeriesEpisode(
-        "Under the sea season 3",
-        "26",
-        //"https://nftjapan.s3.ap-southeast-1.amazonaws.com/image/f7cee056-a816-439b-a37e-5cdbdebdd132-AriumWeb1x1.008.png",
-        "https://files.catbox.moe/s2qswd.jpg",
-        921,
-        1995));
+import 'package:get/get.dart';
+
+import '../../main.dart';
+import '../../model/Serie.dart';
+import '../../model/custom_dio.dart';
+
+class SerieDetailController extends GetxController {
+  final int limit = 8;
+  RxList<SeriesEpisode> episodes = <SeriesEpisode>[].obs;
+
+  void initEpisodes(List<SeriesEpisode> initEpisodes) {
+    episodes.value = initEpisodes;
+    //episodes.refresh();
   }
 
-  return Series(
-      "Sky on the garden",
-      "Light Wealthy Awkward Spite Ice Angelic Pleasant Entry Only Bright Far-off Decisive Argument Formal Shock Acrobatic Thing Primary Previous Building Illustrious Reach Interest Park Efficiency Optimistic Boss Present Opportunity Cabinet Beat Lead Velvety Nonstop Forthright High Enraged Flawed Voice Double ",
-      "https://nftjapan.s3.ap-southeast-1.amazonaws.com/image/346fa06b-c795-405d-b12c-63b8c21860d7-AriumWeb1x1.028.png",
-      "https://nftjapan.s3.ap-southeast-1.amazonaws.com/image/f0c37c95-af79-42d4-bd96-5888181031e8-AriumWeb20x9.004.png",
-      60,
-      1000,
-      "Drama",
-      testEpisodes,
-      "Author Name",
-      "https://files.catbox.moe/8sv2t3.jpg");
+  Future getEpisodes(String serieId, int page) async {
+    CustomDio customDio = CustomDio();
+    customDio.dio.options.headers["Authorization"] =
+        globalController.user.value.certificate.toString();
+    var response = await customDio
+        .get("/serie/$serieId?guest=true&page=$page&limit=$limit");
+    response = jsonDecode(response.toString());
+    var episodesData = response["data"];
+    episodes.value = List.generate(
+        episodesData["episodes"].length,
+        (index) => SeriesEpisode(
+            episodesData["episodes"][index]["name"],
+            episodesData["episodes"][index]["thumbnail"],
+            episodesData["episodes"][index]["price"],
+            episodesData["episodes"][index]["likeInit"],
+            episodesData["episodes"][index]["comments"]));
+    episodes.refresh();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,6 @@ import 'package:untitled/controller/global_controller.dart';
 import 'package:untitled/i18n.dart';
 import 'package:untitled/screen/series_detail/series_detail_screen.dart';
 
-import 'controller/series_detail/series_detail_controller.dart';
-
 GlobalController globalController = Get.put(GlobalController());
 
 Future<void> main() async {
@@ -41,7 +39,9 @@ class MyApp extends StatelessWidget {
           fontFamily: "DMSans",
         ),
         //home: LoginScreen(),
-        home: SeriesDetailScreen(seriesInfo: buildSerie()),
+        home: SeriesDetailScreen(
+          serieId: "5b81de1c-13a9-11ec-932a-87f66c622227",
+        ),
       );
     });
   }
