2eefe3191e8e66e3e5045a7fbd266994eee2a876
Benedict Howard <hongha137200110@gmail.com>
2022-05-01 23:39:18 +0700

    add api initialization + pagination for series detail screen

---
+++ lib/screen/series_detail/search_episodes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class SearchEpisodesScreen extends StatefulWidget {
+  const SearchEpisodesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SearchEpisodesScreen> createState() => _SearchEpisodesScreenState();
+}
+
+class _SearchEpisodesScreenState extends State<SearchEpisodesScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
