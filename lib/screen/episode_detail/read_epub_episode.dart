20fcb0e9f09fbd074af91dc0c7dec81a1d92730a
Benedict Howard <hongha137200110@gmail.com>
2022-05-16 17:01:52 +0700

    add epub reader

--- lib/screen/episode_detail/read_epub_episode.dart
+++ lib/screen/episode_detail/read_epub_episode.dart
@@ -1,6 +1,7 @@
-import 'package:epub_view/epub_view.dart';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:internet_file/internet_file.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class ReadEpubScreen extends StatefulWidget {
   final String episodeKey;
@@ -15,84 +16,31 @@ class ReadEpubScreen extends StatefulWidget {
 }
 
 class _ReadEpubScreenState extends State<ReadEpubScreen> {
-  late EpubController _epubController;
-
-  Future fetchEpisode() async {
-    var doc = await InternetFile.get(
-        'https://webtoonz.uetbc.xyz/uploads/${widget.episodeKey}');
-    _epubController = EpubController(document: EpubDocument.openData(doc));
-  }
-
   @override
-  void dispose() {
-    _epubController.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) WebView.platform = SurfaceAndroidWebView();
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: fetchEpisode(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Scaffold(
-              appBar: AppBar(
-                title: EpubViewActualChapter(
-                  controller: _epubController,
-                  builder: (chapterValue) => Text(
-                    chapterValue?.chapter?.Title?.replaceAll('\n', '').trim() ??
-                        '',
-                    textAlign: TextAlign.start,
-                  ),
-                ),
-                actions: <Widget>[
-                  IconButton(
-                    icon: const Icon(Icons.save_alt),
-                    color: Colors.white,
-                    onPressed: () => _showCurrentEpubCfi(context),
-                  ),
-                ],
-              ),
-              drawer: Drawer(
-                child: EpubViewTableOfContents(controller: _epubController),
-              ),
-              body: EpubView(
-                builders: EpubViewBuilders<DefaultBuilderOptions>(
-                  options: const DefaultBuilderOptions(),
-                  chapterDividerBuilder: (_) => const Divider(),
-                ),
-                controller: _epubController,
-              ),
-            );
-          } else if (snapshot.hasError)
-            return Column(
-              children: [
-                Text(
-                  snapshot.error.toString(),
-                ),
-              ],
-            );
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        });
-  }
-
-  void _showCurrentEpubCfi(context) {
-    final cfi = _epubController.generateEpubCfi();
-
-    if (cfi != null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(cfi),
-          action: SnackBarAction(
-            label: 'GO',
-            onPressed: () {
-              _epubController.gotoEpubCfi(cfi);
-            },
-          ),
-        ),
-      );
-    }
+    var encodedUrl = Uri.encodeFull(
+        "https://webtoonz-s3-v2.uetbc.xyz/?book=${widget.episodeKey}");
+    print(encodedUrl);
+    print(widget.episodeTitle);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.episodeTitle),
+        centerTitle: true,
+      ),
+      body: WebView(
+        initialUrl: encodedUrl,
+        javascriptMode: JavascriptMode.unrestricted,
+        onProgress: (int progress) {
+          print('WebView is loading (progress : $progress%)');
+        },
+        gestureNavigationEnabled: true,
+      ),
+    );
   }
 }
